bf453a17d4164642c590a38f3722ea30f63dae96
huo yongfei <773459418@qq.com>
2019-04-11 13:00:57 +0800

    add test hook

---
+++ test/react/hooks/useRef.test.js
@@ -0,0 +1,29 @@
+import { useRef, useImperativeHandle } from 'react';
+import { renderHook, cleanup } from 'react-hooks-testing-library';
+
+describe('useHook tests', () => {
+  afterEach(cleanup);
+
+  test('should handle useRef hook', () => {
+    const { result } = renderHook(() => useRef());
+
+    const refContainer = result.current;
+
+    expect(Object.keys(refContainer)).toEqual(['current']);
+    expect(refContainer.current).toBeUndefined();
+  });
+
+  test('should handle useImperativeHandle hook', () => {
+    const { result } = renderHook(() => {
+      const ref = useRef();
+      useImperativeHandle(ref, () => ({
+        fakeImperativeMethod: () => true
+      }));
+      return ref;
+    });
+
+    const refContainer = result.current;
+
+    expect(refContainer.current.fakeImperativeMethod()).toBe(true);
+  });
+});
---
+++ test/react/hooks/useState.test.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { renderHook, cleanup, act } from 'react-hooks-testing-library';
+
+describe('useState tests', () => {
+  afterEach(cleanup);
+
+  test('should use setState value', () => {
+    const { result } = renderHook(() => useState('foo'));
+
+    const [value] = result.current;
+
+    expect(value).toBe('foo');
+  });
+
+  test('should update setState value using setter', () => {
+    const { result } = renderHook(() => useState('foo'));
+
+    const [_, setValue] = result.current;
+
+    act(() => setValue('bar'));
+
+    const [value] = result.current;
+
+    expect(value).toBe('bar');
+  });
+});
