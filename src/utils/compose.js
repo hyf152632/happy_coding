aac7fd435acce2e8574205c56149faa095b8442a
huo yongfei <773459418@qq.com>
2019-04-17 13:13:05 +0800

    edit compose fun

--- src/utils/compose.js
+++ src/utils/compose.js
@@ -1,4 +1,4 @@
-const compose = (...fns) => (...args) =>
-  fns.reduce((ret, fn, index) => (index === 0 ? fn(...ret) : fn(ret)), args)
-
-export default compose
+// const compose = (...fns) => (...args) =>
+//   fns.reduce((ret, fn, index) => (index === 0 ? fn(...ret) : fn(ret)), args)
+const compose = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)));
+export default compose;
--- test/utils/compose.test.js
+++ test/utils/compose.test.js
@@ -1,31 +1,31 @@
-import compose from '@src/utils/compose'
+import compose from '@src/utils/compose';
 
 describe('test compose func', () => {
   test('only one fun', () => {
-    const map = a => a
-    expect(compose(map)(1)).toBe(1)
-  })
+    const map = a => a;
+    expect(compose(map)(1)).toBe(1);
+  });
   test('5 and 2 and 3, return 10', () => {
-    const add = (a, b) => a + b
-    const add2 = add.bind(null, 2)
-    const add3 = add.bind(null, 3)
+    const add = (a, b) => a + b;
+    const add2 = add.bind(null, 2);
+    const add3 = add.bind(null, 3);
     expect(
       compose(
         add2,
         add3
       )(5)
-    ).toBe(10)
-  })
+    ).toBe(10);
+  });
   test('first fun can accept multi params', () => {
-    const add = (a, b) => a + b
-    const add2 = add.bind(null, 2)
-    const add3 = add.bind(null, 3)
+    const add = (a, b) => a + b;
+    const add2 = add.bind(null, 2);
+    const add3 = add.bind(null, 3);
     expect(
       compose(
         add,
         add2,
         add3
       )(2, 3)
-    ).toBe(10)
-  })
-})
+    ).toBe(10);
+  });
+});
