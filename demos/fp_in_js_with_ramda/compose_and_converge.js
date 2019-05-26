862986d90752193ec85ae571d8919228189f4711
huo yongfei <773459418@qq.com>
2019-05-26 20:53:40 +0800

    add ramda example

---
+++ demos/fp_in_js_with_ramda/compose_and_converge.js
@@ -0,0 +1,21 @@
+import R from 'ramda'
+
+const person = {
+  id: 1,
+  name: 'Joe'
+}
+
+const generateUrl = id => `https://img.socialnetwork.com/avatar/${id}.png`
+
+const getUrlFromPerson = R.compose(
+  generateUrl,
+  R.propOr('default', 'id')
+)
+
+// const getUpdatePerson = person => R.assoc('avatar', getUrlFromPerson(person), person)
+
+const getUpdatePerson = R.converge(R.assoc('avatar'), [getUrlFromPerson, R.identity])
+
+const result = getUpdatePerson(person)
+
+// console.log(result)
