11fbb8830a211f975ddc9285a1414945dee88099
huo yongfei <773459418@qq.com>
2019-07-04 22:58:18 +0800

    add thinking like programmer

--- books/think_like_a_programmer/demo.js
+++ books/think_like_a_programmer/demo.js
@@ -11,3 +11,9 @@ const getModalNumber = (arr = []) =>
   ).filter(([key, val], ind, arr) => Math.max(...arr.map(([_, v]) => v)) === val)[0][0]
 
 console.log(getModalNumber([1, 2, 3, 3, 4]))
+
+// calculate array int sum
+const sumOfArrIntEle = (arr = [], length = 0, sum = 0) =>
+  length === 0 ? sum : sumOfArrIntEle(arr.slice(1), length - 1, sum + arr[0])
+
+console.log(sumOfArrIntEle([1, 2, 3, 4, 6], 5))
